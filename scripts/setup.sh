5b869731b1d1859b0a01f4e25a1b47156be176ba
guille <guillermopolito@gmail.com>
2016-10-24 12:39:24 +0200

    fixed password for sodbxtest user in postgresql

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@ sqlite3)
   ./pharo Pharo.image eval --save "GAContinuousIntegrationConfiguration new configureSqlite3"
   ;;
 postgresV2)
-  psql -c 'create user sodbxtest createdb createuser password 'postgres';' -U postgres;
+  psql -c 'create user sodbxtest createdb createuser password "sodbxtest";' -U postgres;
   psql -c 'alter user postgres password 'postgres';' -U postgres;
   ./pharo Pharo.image eval --save "GAContinuousIntegrationConfiguration new configurePostgresql";
   ;;
