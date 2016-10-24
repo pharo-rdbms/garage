a9752d1259a0732f8dcdf3b7a3bb357cd3ebcde8
guille <guillermopolito@gmail.com>
2016-10-24 12:34:49 +0200

    made if statement case statement

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,13 @@
-if [ "$1" = "sqlite3" ]
-then
-  ./pharo Pharo.image eval --save "GAContinuousIntegrationConfiguration new configureSqlite3";
-elif [ "$1" = "postgresV2" ]
-then
+case $1 in
+sqlite3)
+  ./pharo Pharo.image eval --save "GAContinuousIntegrationConfiguration new configureSqlite3"
+  ;;
+postgresV2)
   psql -c 'create user sodbxtest createdb createuser password 'postgres';' -U postgres;
   psql -c 'alter user postgres password 'postgres';' -U postgres;
   ./pharo Pharo.image eval --save "GAContinuousIntegrationConfiguration new configurePostgresql";
-else
-  echo "Unsupported database ${1}";
-fi
+  ;;
+*)
+  echo "Unsupported database $1"
+  ;;
+esac
