5998b31275a4747c5bb043e41eeb8c98492f818f
guille <guillermopolito@gmail.com>
2016-10-24 12:21:08 +0200

    removed shebang from setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/bash
-
 if [[ "$1" == "sqlite3" ]]; then
   ./pharo Pharo.image eval --save "GAContinuousIntegrationConfiguration new configureSqlite3";
 elif [[ "$1" == "postgresV2"]] then
