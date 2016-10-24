99bba1f148f4e780d7ea2d5b32f483bd3aa8ed4f
guille <guillermopolito@gmail.com>
2016-10-24 12:27:59 +0200

    fixed if statement

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,11 @@
-if [[ "$1" == "sqlite3" ]]; then
+if [ "$1" = "sqlite3" ]
+then
   ./pharo Pharo.image eval --save "GAContinuousIntegrationConfiguration new configureSqlite3";
-elif [[ "$1" == "postgresV2"]] then
+elif [ "$1" = "postgresV2" ]
+then
   psql -c 'create user sodbxtest createdb createuser password 'postgres';' -U postgres;
   psql -c 'alter user postgres password 'postgres';' -U postgres;
   ./pharo Pharo.image eval --save "GAContinuousIntegrationConfiguration new configurePostgresql";
 else
   echo "Unsupported database ${1}";
-fi;
+fi
