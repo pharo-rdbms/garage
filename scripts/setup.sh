f72b89b375e3ee27361cee66583ff4a14d6c75d2
guille <guillermopolito@gmail.com>
2016-11-02 11:36:32 +0100

    adding mysql job

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,10 @@ case $1 in
 sqlite3)
   ./pharo Pharo.image eval --save "GAContinuousIntegrationConfiguration new configureSqlite3"
   ;;
+mysql)
+  mysql -uroot -p -e "CREATE USER sodbxtest@localhost IDENTIFIED BY 'sodbxtest';"
+  ./pharo Pharo.image eval --save "GAContinuousIntegrationConfiguration new configureMysql"
+  ;;
 postgresV2)
   psql -c "create user sodbxtest createdb createuser password 'sodbxtest';" -U postgres;
   psql -c "alter user postgres password 'postgres';" -U postgres;
